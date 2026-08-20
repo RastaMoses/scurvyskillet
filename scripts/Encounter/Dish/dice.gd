ad2173d11426a83273df1387382c43eea89613d3
Leon <leon@nyoucomer.de>
2026-08-20 13:54:44 +0200

    cube animations

--- scripts/Encounter/Dish/dice.gd
+++ scripts/Encounter/Dish/dice.gd
@@ -4,17 +4,23 @@ extends RigidBody2D
 @export var random_power:Vector2
 @export var max_speed = 300
 @export var highlight_duration:float = 5
+@export var sizzle_scene:PackedScene
+@export var sizzle_interval:float = 1
 #CACHED COMPS
 @onready var collision_shape = $CollisionShape2D
 @onready var sprite = $Sprite2D
 @onready var highlight_sprite = $highlight
 @onready var random = RandomNumberGenerator.new()
+@onready var dice_disp = get_parent()
 
 #STATE
 var move_dir
 var move_power
 var highlight_time:float
 var highlighted:bool = false
+#sizzle
+var sizzle_active = true
+var sizzle_timer = sizzle_interval
 
 #Signals
 
@@ -34,14 +40,23 @@ func change_sprite(texture):
 func stop_movement():
 	linear_velocity = Vector2.ZERO
 	freeze = true
+	sizzle_active = false
 
 func _process(delta: float) -> void:
 	if linear_velocity.length() > max_speed:
 		linear_velocity *= 0.9
+	
 	if highlighted:
 		highlight_time -= delta
 		highlight_sprite.self_modulate.a = highlight_time/highlight_duration
-		
+	if sizzle_active:
+		if sizzle_timer >= sizzle_interval:
+			sizzle_timer = 0
+			var anim = sizzle_scene.instantiate()
+			dice_disp.add_child(anim)
+			anim.global_position = global_position
+			anim.global_rotation = linear_velocity.angle()
+		sizzle_timer += delta
 
 func start_highlight():
 	highlighted = true
---
+++ scripts/Encounter/Dish/sizzle_obj.gd
@@ -0,0 +1,23 @@
+extends AnimatedSprite2D
+
+@export var pixel_multiple: int = 8  # Change to 4, 8, etc. as needed
+
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var raw_position: Vector2 = global_position 
+	# Snap to multiples of your chosen pixel size
+	global_position = Vector2(
+		round(raw_position.x / pixel_multiple) * pixel_multiple,
+		round(raw_position.y / pixel_multiple) * pixel_multiple
+	)
+	await get_tree().process_frame
+	
+	# Snap 2D rotation degrees to the nearest 90-degree increment
+	global_rotation_degrees = snappedf(global_rotation_degrees, 90.0)
+	play("sizzle")
+
+
+func _on_animation_finished() -> void:
+	queue_free()
