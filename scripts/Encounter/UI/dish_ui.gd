282cd21e8d2ed24ce8d90117522ca803e29ce11e
Leon <leon@nyoucomer.de>
2026-08-22 03:01:25 +0200

    plate animation

--- scripts/Encounter/UI/dish_ui.gd
+++ scripts/Encounter/UI/dish_ui.gd
@@ -1,6 +1,7 @@
 extends Control
 @export var result_bg_move_x:int = 80
 @export var result_bg_move_duration:float = 1.0
+@export var plate_anim_speed:float = 1
 
 @onready var dish = get_parent()
 @onready var sweet_text = $Results/SweetText
@@ -26,6 +27,10 @@ var sour_bg_moved = false
 var spicy_bg_moved = false
 var hearty_bg_moved = false
 var fresh_bg_moved = false
+
+var plate_anim_active = false
+var active_plates:int = 0
+var current_nutrition:int = 0
 
 func update_flavours():
 	
@@ -89,24 +94,35 @@ func update_flavours():
 		fresh_text.visible = true
 		fresh_bg_moved = false
 
-
 func update_nutrition(new_value: Variant) -> void:
 	nutrition_text.text = str(new_value)
+	current_nutrition = new_value
 	if new_value > 0:
 		nutrition_shadow.visible = true
 		nutrition_text.visible = true
 	else:
 		nutrition_shadow.visible = false
 		nutrition_text.visible = false
-	var temp = 0
-	for i in nutrition_plates:
-		i.visible = false
-		temp += 1
-	for i in clampi(new_value,0,nutrition_plates.size()):
-		nutrition_plates[i].visible = true
 
 func toggle_highlight_pan(value):
 	pan_highlight.visible = value
 
 func _on_drop_area_mouse_exited() -> void:
 	toggle_highlight_pan(false)
+
+func _process(delta: float) -> void:
+	if active_plates != current_nutrition and !plate_anim_active:
+		animate_plates()
+
+func animate_plates():
+	plate_anim_active = true
+	var plate_diff = current_nutrition - active_plates
+	if plate_diff > 0:
+		nutrition_plates[1+active_plates].visible = true
+		await get_tree().create_timer(1.0/plate_anim_speed).timeout
+		active_plates += 1
+	else:
+		nutrition_plates[active_plates].visible = false
+		await get_tree().create_timer(1.0/plate_anim_speed).timeout
+		active_plates -= 1
+	plate_anim_active = false
