fdac1848803515e8f207062f13d03f287b1dce2a
Leon <leon@nyoucomer.de>
2026-08-27 17:24:16 +0200

    better ship movement

--- scripts/Map/ship_mover.gd
+++ scripts/Map/ship_mover.gd
@@ -1,6 +1,7 @@
 extends Node2D
 #Params
-@export var move_time:float = 3.0
+@export var move_time:float = 2.0
+@export var smoothing_curve:float = -3.0
 @export var pixel_multiple:int = 8
 
 #CACHED
@@ -10,12 +11,14 @@ extends Node2D
 signal ship_arrived
 
 #STATE
+var start_pos: Vector2
 var destination:Vector2
 var is_moving = false
 var current_pos = 0
 var time_moved:float
 
 func start_moving(dock_pos, encounter_index):
+	start_pos = global_position
 	destination = dock_pos.global_position
 	current_pos = encounter_index
 	time_moved = 0
@@ -33,8 +36,10 @@ func _process(delta: float) -> void:
 	if is_moving:
 		time_moved += delta / move_time
 		time_moved = clampf(time_moved,0.0,1.0)
-		global_position = lerp(global_position, destination, time_moved)
-		if time_moved >= 1.0:
+		var eased_time = ease(time_moved, smoothing_curve)
+		global_position = lerp(start_pos, destination, eased_time)
+		print(time_moved)
+		if time_moved >= 1:
 			arrive()
 	#visuals
 	var raw_position: Vector2 = global_position 
